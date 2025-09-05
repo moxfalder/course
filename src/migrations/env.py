8fa8d734e7cc047cbfb685eaf901bf6ca67bbf4c
Хваль Евгений Владимирович <hval1988@gmail.com>
2025-09-05 17:01:09 +0300

    Первый запуск Alembic

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,13 +1,12 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+
 
 from src.db import Base
 from src.config import settings
-from src.models import MHotels
+from src.models.hotels import MHotels
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
